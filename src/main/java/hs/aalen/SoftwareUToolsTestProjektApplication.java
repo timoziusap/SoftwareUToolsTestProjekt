020756cc3caa71470c8ede09c67a900f6c6c58c7
Timo Appt <3009368@studmail.htw-aalen.de>
2026-05-19 09:09:48 +0200

    Initial commit

---
+++ src/main/java/hs/aalen/SoftwareUToolsTestProjektApplication.java
@@ -0,0 +1,13 @@
+package hs.aalen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareUToolsTestProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareUToolsTestProjektApplication.class, args);
+	}
+
+}
