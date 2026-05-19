36dedccfad7de19fdac0811f94a955e34d39f475
Timo Appt <3009368@studmail.htw-aalen.de>
2026-05-19 09:16:36 +0200

    Erster Test Kommentar

--- src/main/java/hs/aalen/SoftwareUToolsTestProjektApplication.java
+++ src/main/java/hs/aalen/SoftwareUToolsTestProjektApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SoftwareUToolsTestProjektApplication {
-
+//Hey das hier ist die erste änderung Grüße Timo 
 	public static void main(String[] args) {
 		SpringApplication.run(SoftwareUToolsTestProjektApplication.class, args);
 	}
