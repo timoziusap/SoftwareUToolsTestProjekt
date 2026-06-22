f56db80d92ca9e9dfeaa8296db0af836c95a57b0
Timo Appt <3009368@studmail.htw-aalen.de>
2026-06-22 18:34:07 +0200

    Test

--- src/main/java/hs/aalen/SoftwareUToolsTestProjektApplication.java
+++ src/main/java/hs/aalen/SoftwareUToolsTestProjektApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SoftwareUToolsTestProjektApplication {
 //Hey das hier ist die erste änderung Grüße Timo
-//Moin Leute
+//Moin Leute ddd
 	//Push v. Passi
 	public static void main(String[] args) {
 		SpringApplication.run(SoftwareUToolsTestProjektApplication.class, args);
